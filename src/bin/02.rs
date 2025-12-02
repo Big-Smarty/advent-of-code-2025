cf5c8052d9a9cbb56442f145d2ec3063ba154987
Big-Smarty <budaniasco@gmail.com>
2025-12-02 21:49:20 +0100

    Solved day 2 quiz 2 and parallelized both quizzes

--- src/bin/02.rs
+++ src/bin/02.rs
@@ -1,4 +1,4 @@
-use rayon::iter::{IntoParallelRefIterator, ParallelIterator};
+use rayon::iter::{IntoParallelIterator, IntoParallelRefIterator, ParallelIterator};
 
 advent_of_code::solution!(2);
 
@@ -23,8 +23,16 @@ impl Range {
 
     pub fn get_invalid_sum(&self) -> u64 {
         (self.min..=self.max)
+            .into_par_iter()
             .filter(|i| !check_validity(*i))
-            .fold(0, |count, i| count + i)
+            .sum()
+    }
+
+    pub fn get_invalid_sum_2(&self) -> u64 {
+        (self.min..=self.max)
+            .into_par_iter()
+            .filter(|i| !check_validity_2(*i))
+            .sum()
     }
 }
 
@@ -42,6 +50,34 @@ fn check_validity(id: u64) -> bool {
     }
 }
 
+fn check_validity_2(id: u64) -> bool {
+    let id_str = id.to_string();
+
+    // go from 1 to half of string length (anything more would be a waste of time)
+    (1..=id_str.len() / 2)
+        .into_iter()
+        .map(|i| {
+            // OPTIMIZATION: skip current iteration if the id length is not divisible by i
+            if id_str.len() % i != 0 {
+                return true;
+            } else {
+                // iterate through all chunks; if all chunks are equal to the first chunk, the id is invalid!
+                if id_str.as_bytes().chunks(i).all(|x| {
+                    // check if current chunk is equal to first chunk
+                    *x == id_str.as_bytes()[0..i]
+                }) {
+                    // id is invalid; all chunks were equal
+                    //println!("invalid id: {id}");
+                    return false;
+                    // id is valid; not all chunks were equal
+                } else {
+                    return true;
+                }
+            }
+        })
+        .fold(true, |out, b| out & b)
+}
+
 fn get_range_vec(input: &str) -> Vec<Range> {
     input
         .replace("\n", "")
@@ -57,7 +93,14 @@ pub fn part_one(input: &str) -> Option<u64> {
 }
 
 pub fn part_two(input: &str) -> Option<u64> {
-    None
+    //let input = "11-22,95-115,998-1012,1188511880-1188511890,222220-222224,1698522-1698528,446443-446449,38593856-38593862,565653-565659,824824821-824824827,2121212118-2121212124";
+    let ranges = get_range_vec(input);
+    Some(
+        ranges
+            .par_iter()
+            .map(|r| r.get_invalid_sum_2())
+            .sum::<u64>(),
+    )
 }
 
 #[cfg(test)]
