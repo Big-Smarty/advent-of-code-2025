65c904ffb6ac29a2b2cc121b16a6c9913ec99d3c
Big-Smarty <budaniasco@gmail.com>
2025-12-03 22:11:14 +0100

    Finished day 1 quiz 1

--- src/bin/03.rs
+++ src/bin/03.rs
@@ -1,15 +1,44 @@
+use rayon::{iter::ParallelIterator, str::ParallelString};
+
 advent_of_code::solution!(3);
 
 pub fn part_one(input: &str) -> Option<u64> {
+    Some(
+        input
+            .par_lines()
+            .map(|l| {
+                let max = l.chars().map(|c| c as u8 - 48).max().unwrap();
+                if l.chars()
+                    .map(|c| c as u8 - 48)
+                    .position(|y| y == max)
+                    .unwrap() as usize
+                    == l.len() - 1
+                {
+                    let second_max = l[..l.len() - 1]
+                        .chars()
+                        .map(|c| c as u8 - 48)
+                        .max()
+                        .unwrap();
+                    return second_max * 10 + max;
+                } else {
+                    let second_max = l[l.chars().position(|x| x as u8 - 48 == max).unwrap() + 1..]
+                        .chars()
+                        .map(|x| x as u8 - 48)
+                        .max()
+                        .unwrap();
+                    return max * 10 + second_max;
+                }
+            })
+            .map(|i| i as u64)
+            .sum(),
+    )
+}
+
+pub fn part_two(input: &str) -> Option<u64> {
     let input = "987654321111111
 811111111111119
 234234234234278
 818181911112111";
-
-    None
-}
-
-pub fn part_two(input: &str) -> Option<u64> {
     None
 }
 
